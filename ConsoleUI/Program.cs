e5398b64326ec10c55dabd45ee4cb186685aa5d9
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-29 15:03:44 +0200

    Added a summary comment to the consoleui class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,8 +4,22 @@ namespace ConsoleUI
 {
     internal class Program
     {
+            // 1. IEnumerable <T> for generic collections.
+            // 2. IEnumerable for non generic collections.
+
+            /// <summary>
+            /// IEnumerable<T> contains a single method that you must implement when implementing this interface;
+            /// GetEnumerator(), which returns an IEnumerator<T> object.
+            /// The returned IEnumerator<T> provides the ability to interate through the collection
+            /// by exposing a Current property that points at the object we are currently at in the collecion.
+            /// </summary>
+            
+            
+            /// When it is recommended to use the IEnumerable interface:... 
+        
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
