5586014e295a2f8f138ec90bb1c3bcb286a63744
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-29 15:02:51 +0200

    Added logic to the dog class.

--- ShelterLibrary/Dog.cs
+++ ShelterLibrary/Dog.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ShelterLibrary
 {
-    internal class Dog
+    public class Dog
     {
+        // The name of the dog.
+        public string Name { get; set; }
+        // Is this a naughty dog.
+        public bool IsNaughtyDog { get; set; }
+        // Simple constructor.
+        public Dog(string name, bool isNaughtyDog) 
+        {
+            this.Name = name;
+            this.IsNaughtyDog = isNaughtyDog;
+        }
+
+        // This method will print how many treats the dog received.
+        public void GiveTreat(int numberOfTreats)
+        {
+            // Print a message containing the number of treats and the name of the dog.
+            Console.WriteLine($"Dog: {Name} said wuoff {numberOfTreats}!.");
+        }
     }
 }
