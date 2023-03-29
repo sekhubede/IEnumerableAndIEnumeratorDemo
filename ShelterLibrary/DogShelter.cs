2a1b8b8fea3efb501c7a4d9fd324ad0ed3b4ee1d
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-29 15:40:35 +0200

    Added logic to the DogShelter class.

--- ShelterLibrary/DogShelter.cs
+++ ShelterLibrary/DogShelter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,32 @@ using System.Threading.Tasks;
 
 namespace ShelterLibrary
 {
-    public class DogShelter
+    // A class named DogShelter this class contains a generic collection of type Dog
+    // objects of this class can't be used inside a for each loop because it lacks an implementtion of the IEnumerable interface.
+    public class DogShelter : IEnumerable<Dog>
     {
+        // List of type List<Dog>
+        public List<Dog> dogs;
+        // This constructor will initialize the dogs list with some values.
+        public DogShelter()
+        {
+            dogs = new List<Dog>()
+            {
+                new Dog("Casper",false),
+                new Dog("Sif", true),
+                new Dog("Oreo",false),
+                new Dog("Pixel",false),
+            };
+        }
 
+        IEnumerator<Dog> IEnumerable<Dog>.GetEnumerator()
+        {
+            return dogs.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
