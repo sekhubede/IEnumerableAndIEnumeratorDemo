b1fece4c8e9860fc6e45ae43f4ecc644f3f6f096
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-30 15:02:20 +0200

    Added a CollectionSum add some logic to the Program class
    Passing an IEnumerable to a method and summing up the collection's values.

---
+++ ConsoleUI3/Program.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace ConsoleUI3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A list of type List<int> intialized with some number.
+            List<int> numberList = new List<int>() { 8, 6, 2 };
+
+            // An array of type int[] intialized with some numbers.
+            int[] numberArray = new int[] { 1, 7, 1, 3 };
+
+            // New line.
+            WriteLine();
+
+            // Call CollectionSum() and pass the list to it.
+            CollectionSum(numberList);
+
+            // New line.
+            WriteLine();
+
+            // Call CollectionSum() and pass the array to it.
+            CollectionSum(numberArray);
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection) 
+        {
+            // Sum variable to store the sum of the numbers in anyCollection.
+            int sum = 0;
+
+            // foreach number in the collection passed to this method.
+            foreach (int num in anyCollection)
+            {
+                // Add the num value to sum.
+                sum += num;
+            }
+
+            Write($"Sum is {sum}");
+        }
+    }
+}
